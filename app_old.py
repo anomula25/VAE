43e5aa7747c99da4768f371cb6ce3f69b8efd4bf
anomula25 <57620742+anomula25@users.noreply.github.com>
2024-04-07 15:39:51 -0400

    Update app_old.py

--- app_old.py
+++ app_old.py
@@ -1,40 +1,3 @@
-# import streamlit as st
-# import numpy as np
-# import tensorflow as tf
-# from PIL import Image
-
-# # Load the trained VAE model
-# def load_model():
-#     model = tf.keras.models.load_model('vae_model_v2.h5')  # Replace 'vae_model.h5' with your model file
-#     return model
-
-# vae_model = load_model()
-
-# # Function to generate output from the VAE model
-# def generate_output(input_image):
-#     # Preprocess the input image (resize, normalize, etc.)
-#     input_image = np.array(input_image.resize((64, 64))) / 255.0
-#     input_image = np.expand_dims(input_image, axis=0)
-
-#     # Generate output from the VAE model
-#     output_image = vae_model.predict(input_image)
-
-#     return output_image[0]  # Return the generated output image
-
-# # Streamlit UI
-# st.title('VAE Model Deployment')
-
-# uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
-
-# if uploaded_file is not None:
-#     input_image = Image.open(uploaded_file)
-#     st.image(input_image, caption='Uploaded Image', use_column_width=True)
-#     if st.button('Generate Output'):
-#         with st.spinner('Generating Output...'):
-#             output_image = generate_output(input_image)
-#             st.image(output_image, caption='Generated Output', use_column_width=True)
-#             st.success('Output generated successfully!')
-
 import streamlit as st
 import numpy as np
 import tensorflow as tf
@@ -48,14 +11,24 @@ def load_model():
 vae_model = load_model()
 
 # Function to preprocess input image and generate output from the VAE model
+# def generate_output(input_image):
+#     # Preprocess the input image (resize, normalize, etc.)
+#     input_image = np.array(input_image.resize((128, 128))) / 255.0
+#     input_image = np.expand_dims(input_image, axis=0)
+
+#     # Generate output from the VAE model
+#     #output_image = vae_model(input_image)
+#     output_image = vae_model(input_image)# Assuming your VAE model directly takes input and generates output
+#     return output_image[0]  # Return the generated output image
+
 def generate_output(input_image):
-    # Preprocess the input image (resize, normalize, etc.)
     input_image = np.array(input_image.resize((128, 128))) / 255.0
     input_image = np.expand_dims(input_image, axis=0)
 
-    # Generate output from the VAE model
-    output_image = vae_model(input_image)  # Assuming your VAE model directly takes input and generates output
-    return output_image[0]  # Return the generated output image
+    # Assuming the model exposes a callable serving signature:
+    output_image = vae_model.signatures['serving_default'](tf.convert_to_tensor(input_image))['output_0']
+
+    return output_image[0].numpy()
 
 # Streamlit UI
 st.title('VAE Model Deployment')
