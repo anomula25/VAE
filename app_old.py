51a51379f8010f68e2118581eb3a2b7b6072d187
anomula25 <57620742+anomula25@users.noreply.github.com>
2024-04-07 16:18:00 -0400

    Update app_old.py

--- app_old.py
+++ app_old.py
@@ -10,29 +10,9 @@ def load_model():
 
 vae_model = load_model()
 
-# Function to preprocess input image and generate output from the VAE model
-# def generate_output(input_image):
-#     # Preprocess the input image (resize, normalize, etc.)
-#     input_image = np.array(input_image.resize((128, 128))) / 255.0
-#     input_image = np.expand_dims(input_image, axis=0)
-
-#     # Generate output from the VAE model
-#     #output_image = vae_model(input_image)
-#     output_image = vae_model(input_image)# Assuming your VAE model directly takes input and generates output
-#     return output_image[0]  # Return the generated output image
-
-# def generate_output(input_image):
-#     input_image = np.array(input_image.resize((128, 128))) / 255.0
-#     input_image = np.expand_dims(input_image, axis=0)
-
-#     # Assuming the model exposes a callable serving signature:
-#     output_image = vae_model.signatures['serving_default'](tf.convert_to_tensor(input_image))['output_0']
-
-#     return output_image[0].numpy()
-
 def generate_output(input_image):
     # Resize and normalize the input image
-    input_image = np.array(input_image.resize((128, 128))) / 255.0
+    input_image = np.array(input_image.resize((64, 64))) / 255.0
     input_image = np.expand_dims(input_image, axis=0)
     
     # Ensure the input is in the correct dtype, TensorFlow typically expects float32
@@ -53,15 +33,7 @@ def generate_output(input_image):
 # Streamlit UI
 st.title('VAE Model Deployment')
 
-#uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
-
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png"], accept_multiple_files=False)
-
-# if uploaded_file is not None:
-#     with open(os.path.join("uploads", uploaded_file.name), "wb") as f:
-#         f.write(uploaded_file.getbuffer())
-#     st.success("File successfully uploaded.")
-
 
 if uploaded_file is not None:
     input_image = Image.open(uploaded_file)
